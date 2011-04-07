9b0c438557bc277be592f6a5700b79ec49866067
Seth Milliken <seth@araxia.net>
2011-04-07 10:55:44 -0700

    fix sessionman ref

--- bundle/refresh_bundles.sh
+++ bundle/refresh_bundles.sh
@@ -63,7 +63,7 @@ refresh_bundle project git://github.com/shemerey/vim-project.git
 refresh_bundle rails git://github.com/tpope/vim-rails.git
 refresh_bundle screenshot git://github.com/vim-scripts/ScreenShot.git
 refresh_bundle script-ed ssh://seth@at.araxia.net/~/git/script-ed.git
-refresh_bundle sessionman git://github.com/vim-scripts/sessionman.vim.git
+refresh_bundle sessionman git://github.com/vim-scripts/sessionman.git
 refresh_bundle snipmate git://github.com/spf13/snipmate.vim.git
 refresh_bundle space git://github.com/spiiph/vim-space.git
 refresh_bundle statuslinehighlight git://github.com/vim-scripts/StatusLineHighlight.git
